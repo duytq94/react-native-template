b1af67e8d2cda2ff461aa9670bed5ca14fea4bb1
Duy Tran <tranquangduy7994@gmail.com>
2020-02-25 13:43:52 +0700

    remove some unused and change stateless component to function component

--- src/Components/NoDataView.js
+++ src/Components/NoDataView.js
@@ -1,32 +1,25 @@
-import React, {Component} from 'react'
-import {Text, TouchableOpacity, View} from 'react-native'
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import {fontFamily, fontSize} from "../const";
+import React from 'react'
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native'
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+import {fontFamily, fontSize} from "../const"
 import colors from "../Themes/Colors"
 
-export default class NoDataView extends Component {
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return false
-    }
-
-    render() {
-        return (
-            <View style={styles.viewNoData}>
-                <MaterialCommunityIcons name={'information-outline'} size={64} color={colors.grey}/>
-                <Text style={styles.textNoData}>No data</Text>
-                <TouchableOpacity
-                    style={styles.btnRetry}
-                    onPress={this.props.onRetryPress}
-                >
-                    <Text style={styles.textRetry}>Try again</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+const NoDataView = (props) => {
+    return (
+        <View style={styles.viewNoData}>
+            <MaterialCommunityIcons name={'information-outline'} size={64} color={colors.grey}/>
+            <Text style={styles.textNoData}>No data</Text>
+            <TouchableOpacity
+                style={styles.btnRetry}
+                onPress={props.onRetryPress}
+            >
+                <Text style={styles.textRetry}>Try again</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
 
-const styles = {
+const styles = StyleSheet.create({
     viewNoData: {
         flex: 1,
         alignItems: 'center',
@@ -53,4 +46,6 @@ const styles = {
         fontFamily: fontFamily.demiBold,
         color: colors.primary,
     }
-}
+})
+
+export default NoDataView
--- src/Themes/index.js
+++
@@ -1,4 +0,0 @@
-import Colors from './Colors'
-import ApplicationStyle from './Application.Style'
-
-export {Colors, ApplicationStyle}
