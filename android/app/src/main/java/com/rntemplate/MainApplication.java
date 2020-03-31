0985a4ebbc6d33ce95e3e822182e780fd1931063
duytq94 <tranquangduy7994@gmail.com>
2020-03-31 11:10:19 +0700

    upgrade to RN 0.62 https://github.com/facebook/react-native/issues/28406#issuecomment-604792832

--- android/app/src/main/java/com/rntemplate/MainApplication.java
+++ android/app/src/main/java/com/rntemplate/MainApplication.java
@@ -1,7 +1,9 @@
 package com.rntemplate;
 
 import android.app.Application;
+import android.content.Context;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -12,6 +14,7 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -50,5 +53,26 @@ public class MainApplication extends Application implements ReactApplication {
     public void onCreate() {
         super.onCreate();
         SoLoader.init(this, /* native exopackage */ false);
+        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    }
+
+    private static void initializeFlipper(
+            Context context, ReactInstanceManager reactInstanceManager) {
+        if (BuildConfig.DEBUG) {
+            try {
+                Class<?> aClass = Class.forName("com.rntemplate.ReactNativeFlipper");
+                aClass
+                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                        .invoke(null, context, reactInstanceManager);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ ios/Dummy.swift
@@ -0,0 +1,9 @@
+//
+//  Dummy.swift
+//  RNTemplate
+//
+//  Created by LAP01434 on 3/31/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
